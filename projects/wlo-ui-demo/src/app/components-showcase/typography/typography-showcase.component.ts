3606c9ec48220d9ad515c9faeed52c9702373102
Willian Oliveira <willian.oliveira@creationit.com.br>
2025-04-02 10:35:05 -0300

    Add TypographyShowcase component with HTML template and styles for showcasing WloTypography variants

---
+++ projects/wlo-ui-demo/src/app/components-showcase/typography/typography-showcase.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { WloTypographyComponent } from 'wlo-ui';
+
+@Component({
+  standalone: true,
+  selector: 'app-typography-showcase',
+  imports: [WloTypographyComponent],
+  templateUrl: './typography-showcase.component.html',
+})
+export class TypographyShowcaseComponent {}
