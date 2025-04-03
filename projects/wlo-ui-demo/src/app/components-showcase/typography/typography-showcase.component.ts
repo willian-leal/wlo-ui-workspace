f07b2d30f12cc31a7a5908115c3b29ef2a4de67c
Willian Oliveira <willian.oliveira@creationit.com.br>
2025-04-02 23:30:54 -0300

    Add MatTabsModule to TypographyShowcaseComponent and create associated styles

--- projects/wlo-ui-demo/src/app/components-showcase/typography/typography-showcase.component.ts
+++ projects/wlo-ui-demo/src/app/components-showcase/typography/typography-showcase.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { WloTypographyComponent } from 'wlo-ui';
 
 @Component({
   standalone: true,
   selector: 'app-typography-showcase',
-  imports: [WloTypographyComponent],
+  imports: [WloTypographyComponent, MatTabsModule],
   templateUrl: './typography-showcase.component.html',
+  styleUrl: './typography-showcase.component.scss'
 })
 export class TypographyShowcaseComponent {}
