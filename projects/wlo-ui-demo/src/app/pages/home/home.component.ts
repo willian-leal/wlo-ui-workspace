db37a432546efd9b4eb01703929e63091a5a1b13
Willian Oliveira <willian.oliveira@creationit.com.br>
2025-04-03 22:56:02 -0300

    Update button icons and enhance home component layout for better dark mode support

--- projects/wlo-ui-demo/src/app/pages/home/home.component.ts
+++ projects/wlo-ui-demo/src/app/pages/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { WloButtonComponent, WloTypographyComponent } from 'wlo-ui';
+import { WloTypographyComponent } from 'wlo-ui';
 import {MatTabsModule} from '@angular/material/tabs';
 import { RouterLink } from '@angular/router';
+import { WloUiModule } from "../../../../../wlo-ui/src/lib/wlo-ui.module";
+import { ThemeService } from '@wlo/theme/theme.service';
 
 @Component({
   standalone: true,
-  selector: 'app-home',
-  imports: [MatTabsModule, RouterLink, WloTypographyComponent],
+  imports: [MatTabsModule, RouterLink, WloTypographyComponent, WloUiModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  constructor(private theme: ThemeService) {}
+
+  get isDarkMode(): boolean {
+    return this.theme.getTheme() === 'dark';
+  }
+}
